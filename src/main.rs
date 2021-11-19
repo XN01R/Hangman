07dc88616636fbbcfb977cc6dee8fdc1e58e8d08
XN01R <93876544+XN01R@users.noreply.github.com>
2021-11-19 16:49:37 -0500

    Update main.rs
    - Added a random word generator 
    - User can now properly guess the word

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,80 @@
 use std::io;
-use std::io::Write;
+use rand::Rng;
+
 
 fn main() {
 
-    let mut limbs = 5;
-    let secret_word = "hello";
-    let mut guess = String::new();
+    let mut limbs = 4;
+    let num_gen = rand::thread_rng().gen_range(0..3);
+    let secret_word = words()[num_gen];
 
-    println!("\t\t== !Hangman! ==\n");
-    println!("Word is {} characters long", secret_word.len());
-    println!("You have {} limbs remaining\n", limbs);
+    println!("\n\t\t== !Hangman! ==\n");
+    println!("Random word generated.");
+    println!("You have {} limbs.", limbs);
 
     loop {
-        print!("Enter guess: ");
 
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let mut guess = String::new();
+
+        for _c in secret_word.chars() {
+            print!(" _");
+        }
+
+        println!("\nEnter guess: ");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
         let guess = guess.trim().to_lowercase();
 
 
-        if guess == secret_word
-        {
-            println!("You guessed it!");
-            break;
+        if guess.len() == 1 {
+
+            if secret_word.contains(&guess) {
+                println!("{} is in the word!", guess);
+                continue; 
+            }
+
+            else if !secret_word.contains(&guess) {
+                println!("Not in word");
+                limbs -= 1;
+
+                println!("{} not in word. {} limbs remaining", guess, limbs);
+
+                if limbs == 0 {
+                        println!("You died!");
+                        break;
+                }
+            }
         }
 
-        else if guess != secret_word
-        {
-            limbs -= 1;
 
-            if limbs == 0
-            {
-                println!("You died!");
+        else if guess.len() > 1 {
+            if guess == secret_word {
+                println!("You guessed it!");
                 break;
             }
 
-            println!("Incorrect guess, you have {} limbs remaining\n", limbs);
+            else if guess != secret_word {
+                limbs -= 1;
+                println!("Incorrect guess! {} limbs remaining", limbs);
+            
+
+                if limbs == 0 {
+                    println!("You died!");
+                    break;
+                }
+            }
         }
     }
+}
+
+
+fn words() -> [&'static str; 4] {
+
+
+    ["hello", "bye", "morning", "midnight"]
+
+    
 }
